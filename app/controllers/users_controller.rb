eb7473a6062694c102859c50814a2473088c4dcb
Isashi <isashimitsui@gmail.com>
2017-09-11 10:13:56 +0200

    basic sign up with name and email

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,5 +11,10 @@ class UsersController < ApplicationController
 
  	def show
  		@user = User.find(params[:id])
-	end 
+	end
+
+	private
+	    def user_params
+      params.require(:user).permit(:name, :email)
+    end 
 end
